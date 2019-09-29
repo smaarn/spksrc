6d65c660c1ff1e3eced24caf6dada7009a28c719
Smaarn <antoine.malliarakis@gmail.com>
2019-09-29 15:00:05 +0200

    spk/bazarr: 0.9.0.4

---
+++ spk/bazarr/src/service-setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Package
+PACKAGE="bazarr"
+DNAME="Bazarr"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python3"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+PATH="${INSTALL_DIR}/env/bin:${INSTALL_DIR}/bin:${PYTHON_DIR}/bin:${PATH}"
+VAR_DIR="${INSTALL_DIR}/var"
+PYTHON3="${PYTHON_DIR}/bin/python3"
+
+SC_USER="sc-${PACKAGE}"
+GROUP="sc-download"
+
+service_postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR} >> "${INST_LOG}" 2>&1
+
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env >> ${INST_LOG} 2>&1
+
+    # Install the wheels
+    ${INSTALL_DIR}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${INSTALL_DIR}/share/wheelhouse ${INSTALL_DIR}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+
+    mkdir -p "${VAR_DIR}/data"  >> "${INST_LOG}" 2>&1
+    
+    set_unix_permissions "${VAR_DIR}" >> "${INST_LOG}" 2>&1
+    
+    set_unix_permissions "${VAR_DIR}/data" >> "${INST_LOG}" 2>&1
+
+    echo "Running busybox installation..."  >> "${INST_LOG}" 2>&1
+
+    # Install busybox
+    ${PYTHON_DIR}/bin/busybox --install ${INSTALL_DIR}/bin >> "${INST_LOG}" 2>&1
+
+}
