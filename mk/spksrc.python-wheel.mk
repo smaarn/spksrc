89321a9cc1afb5c9dc6c9af15a099008df543268
Smaarn <antoine.malliarakis@gmail.com>
2020-04-15 12:08:47 +0200

    [python3] use crossenv to handle cross compilation of wheels

--- mk/spksrc.python-wheel.mk
+++ mk/spksrc.python-wheel.mk
@@ -25,14 +25,15 @@ PYTHONPATH = $(PYTHON_LIB_NATIVE):$(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_D
 
 ### Python wheel rules
 build_python_wheel:
-	@$(RUN) PYTHONPATH=$(PYTHONPATH) $(HOSTPYTHON) -c "import setuptools;__file__='setup.py';exec(compile(open(__file__).read().replace('\r\n', '\n'), __file__, 'exec'))" $(BUILD_ARGS) bdist_wheel -b $(WORK_DIR)/wheelbuild -d $(WORK_DIR)/wheelhouse
+	@. $(CROSSENV) && \
+		$(RUN) PYTHONPATH=$(PYTHONPATH) python -c "import setuptools;__file__='setup.py';exec(compile(open(__file__).read().replace('\r\n', '\n'), __file__, 'exec'))" $(BUILD_ARGS) bdist_wheel -b $(WORK_DIR)/wheelbuild -d $(WORK_DIR)/wheelhouse
 
 install_python_wheel: $(WHEEL_TARGET)
 	@if [ -d "$(WORK_DIR)/wheelhouse" ] ; then \
 		mkdir -p $(STAGING_INSTALL_PREFIX)/share/wheelhouse ; \
 		cd $(WORK_DIR)/wheelhouse && \
 		  for w in *.whl; do \
-		    cp -f $$w $(STAGING_INSTALL_PREFIX)/share/wheelhouse/`echo $$w | cut -d"-" -f -3`-none-any.whl; \
+		    cp -f $$w $(STAGING_INSTALL_PREFIX)/share/wheelhouse/; \
 		  done ; \
 	fi
 
