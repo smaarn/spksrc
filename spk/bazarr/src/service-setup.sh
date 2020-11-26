03de66d1e2b632d456731e83729c1a95dd8bad40
Smaarn <antoine.malliarakis@gmail.com>
2020-11-26 23:28:16 +0100

    bazarr: 0.9.0.7
    Reworked to use /var/packages/${PACKAGE_NAME}/target instead of /usr/local/${PACKAGE_NAME}

--- spk/bazarr/src/service-setup.sh
+++ spk/bazarr/src/service-setup.sh
@@ -2,28 +2,29 @@
 PACKAGE="bazarr"
 
 # Others
-INSTALL_DIR="/usr/local/${PACKAGE}"
 PYTHON_DIR="/usr/local/python3"
-FFMPEG_DIR="/usr/local/ffmpeg"
+
+
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
-export PATH="${INSTALL_DIR}/env/bin:${INSTALL_DIR}/bin:${FFMPEG_DIR}/bin:${PATH}"
-export VAR_DIR="${INSTALL_DIR}/var"
+VAR_DIR="${SYNOPKG_PKGDEST}/var"
 
 SC_USER="sc-${PACKAGE}"
 GROUP="sc-download"
 SVC_BACKGROUND=y
 PID_FILE="${VAR_DIR}/bazarr.pid"
 SVC_WRITE_PID=y
-SVC_CWD="${INSTALL_DIR}/share/${PACKAGE}"
+SVC_CWD="${SYNOPKG_PKGDEST}/share/${PACKAGE}"
 
 service_postinst ()
 {
 
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env >> ${INST_LOG} 2>&1
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
 
     # Install the wheels (using virtual env through PATH)
-    pip install --no-deps --no-index -U --force-reinstall -f ${INSTALL_DIR}/share/wheelhouse ${INSTALL_DIR}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall \
+        -f ${SYNOPKG_PKGDEST}/share/wheelhouse \
+        ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
     mkdir -p "${VAR_DIR}/data"  >> "${INST_LOG}" 2>&1
     
